511b85b5068752a5eb12d62f8d9c8462a27fdad0
BMM Operator <xf06bm@xf06bm-ws3.nsls2.bnl.local>
2026-01-06 14:17:17 -0500

    update pixi.toml for using bmm_tools

--- startup/00-populate-namespace.py
+++ startup/00-populate-namespace.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/xf06bm/.ipython/bmm_tools/src/')
+#sys.path.append('/home/xf06bm/.ipython/bmm_tools/src/')
 sys.path.append('/home/xf06bm/.local/lib/python3.11/site-packages/')
 #sys.path.insert(0, '/home/xf06bm/git/clones/nslsii/')
 
